39d86bcba58b100265befdf12e94701a2915c4b3
johnlaff <joaoaraxaiba@gmail.com>
2024-04-11 00:14:31 -0300

    Adicionado: Função de buscar um elemento no vetor pela sua posição e pelo seu valor e testes das aulas 05 e 06

---
+++ src/com/johnlaff/estruturadados/vetor/teste/Aula05.java
@@ -0,0 +1,16 @@
+package com.johnlaff.estruturadados.vetor.teste;
+
+import com.johnlaff.estruturadados.vetor.Vetor;
+
+public class Aula05 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
+
+        System.out.println(vetor.busca(3));
+    }
+}
